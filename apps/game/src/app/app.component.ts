a6abc65bae27a5eb7b8fcc710a82c2584fd31702
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-02-07 20:47:44 +0100

    chore(scaffolding): create the most simple nx project

---
+++ apps/game/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yokai-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'game';
+}
